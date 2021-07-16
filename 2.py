fdd44667e842042c2a0989c230fbdbdc99e49212
777moneymaker <adeeeq77@gmail.com>
2021-07-16 10:16:11 +0200

    task2

---
+++ 2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+# Task 2
+def my_counter(word: str):
+	if not isinstance(word, str):
+		raise TypeError("Argument is not a str.")
+	
+	word = word.lower()
+	count = dict(Counter(word))
+	count = {char: c for char, c in count.items() if c > 1}
+	return count
+	
+
+
+if __name__ == "__main__":
+	print(my_counter('aBcbA'))
+	# {'a': 2, 'b': 2}
+	print(my_counter("RabarbArka"))
+	# {'r': 3, 'a': 4, 'b': 2}
